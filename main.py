2dedf0fff8d6b68186a7f541672b6a7641a26036
Betty Ongige <ongigebetty@gmail.com>
2026-03-22 05:00:32 -0700

    Food Suggestor v1

---
+++ main.py
@@ -0,0 +1,21 @@
+food = ["Ugali", "Rice","Chapati","Pancakes", "indomie"]
+day_of_the_week =["Monday","Tuesday", "Wednesday","Thursday", "Friday"]
+
+
+def food_suggestion (food):
+	user_input = input("What day is it today?")
+	if user_input in day_of_the_week:
+		index=day_of_the_week.index(user_input)
+
+		suggest=food[index]
+		
+		print ("Today is {user_input} so lets eat {suggestion}")
+	else:
+		print ("Sorry not today")
+
+
+food_suggestion()
+
+
+
+	
---
+++ readme.md
@@ -0,0 +1,6 @@
+Meal Suggestor app
+day of the week 
+food for that day
+
+
+function to show us the food
