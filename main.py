3f8e4621a91223e849860576d5e808d37babee39
Betty Ongige <ongigebetty@gmail.com>
2026-03-22 05:14:27 -0700

    Working Correctly

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ food = ["Ugali", "Rice","Chapati","Pancakes", "indomie"]
 day_of_the_week =["Monday","Tuesday", "Wednesday","Thursday", "Friday"]
 
 
-def food_suggestion (food):
+def food_suggestion ():
 	user_input = input("What day is it today?")
 	if user_input in day_of_the_week:
-		index=day_of_the_week.index(user_input)
+		index = day_of_the_week.index(user_input)
 
-		suggest=food[index]
+		suggest = food[index]
 		
-		print ("Today is {user_input} so lets eat {suggestion}")
+		print (f"Today is {user_input} so lets eat {suggest}!")
 	else:
 		print ("Sorry not today")
 
